401fa37a1cc4927f9959a6fc56cd15c3b109f388
IrynaKhonina <irinahonina@yahoo.com>
2025-05-11 17:45:16 +0300

    08(hw13)/05/25.1

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -85,7 +85,8 @@ const HW13 = () => {
 
     return (
         <div id={'hw13'}>
-            <div className={s2.hwTitle}>Homework #13</div>
+            <div className={s2.hwTitle}>
+                errors</div>
 
             <div className={s2.hw}>
                 <div className={s.buttonsContainer}>
